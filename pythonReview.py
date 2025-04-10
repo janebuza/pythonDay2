bd8434a3784edf663f73ea649bb43dfa74eb591b
Jane Buza <janebuza@Janes-MacBook-Air.local>
2025-04-10 09:14:56 -0400

    Level 4 Complete

--- pythonReview.py
+++ pythonReview.py
@@ -1,1 +1,15 @@
+# Name : Jane Buza
 print("It worked!");
+
+x = None;
+
+example = "I'm a string"; # The variable example is now holding the string value "I'm a string".
+a = 3; # The variable a is now holding the integer value 3.
+b = 4.0; # The variable b is now holding the float value 4.0.
+c = True; # The variable c is now holding the boolean value True.
+d = False; # The variable d is now holding the boolean value False.
+e = "Hey!"; # The variable e is now holding the string value "Hey!".
+f = None; # The variable f is now holding the None value None.
+age = 32; # The variable age is now holding the integer value 32.
+name = "Jane"; # The variable name is now holding the string value "Jane".
+instrument = "guitar"; # The variable instrument is now holding the string value "guitar".
