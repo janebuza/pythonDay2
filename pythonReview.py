65304d4ab5c508ca753d292ba49c40ae5408ef82
Jane Buza <janebuza@Janes-MacBook-Air.local>
2025-04-10 09:56:13 -0400

    Problem 5 Complete

--- pythonReview.py
+++ pythonReview.py
@@ -1,7 +1,12 @@
 # Name : Jane Buza
-print("It worked!");
+
+# print("It worked!");
+
+#------
 
 x = None;
+
+#------
 
 example = "I'm a string"; # The variable example is now holding the string value "I'm a string".
 a = 3; # The variable a is now holding the integer value 3.
@@ -13,3 +18,48 @@ f = None; # The variable f is now holding the None value None.
 age = 32; # The variable age is now holding the integer value 32.
 name = "Jane"; # The variable name is now holding the string value "Jane".
 instrument = "guitar"; # The variable instrument is now holding the string value "guitar".
+
+#------
+
+'''
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _)
+A variable name cannot be any of the Python keywords.
+
+
+$$$Variable = "bad";
+
+'''
+
+#------
+
+# x = type(3);
+# y = type("Hello");
+# z = type(True);
+
+#------
+
+# prompt = "Enter your name:\n";
+# result = input(prompt);
+# print("Hello", result + ",", "how are you?");
+
+#------
+
+# Problem 1:
+# userName = input("What is your name?\n");
+# userAge = input("What is your age? (int value plz)\n");
+# print(userName + ":", type(userName) + ",", userAge + ":", type(userAge));
+
+# Problem 2:
+# num1 = int(input("Enter the first number\n"));
+# num2 = int(input("Enter the second number\n"));
+# if num1 > num2: print(num1, "is greater");
+# elif num2 > num1: print(num2, "is greater");
+# else: print("Equal");
+
+# Problem 5: 
+name = input("Please enter your full name\n");
+while " " in name: name = name.replace(" ", "");
+nameLength = len(name);
+print(nameLength);
